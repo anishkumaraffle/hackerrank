e35cb285762e538b3846d83a1a3f6b1839de770a
Anish <anish@Swatis-Mac-mini.local>
2017-03-23 18:58:52 +0530

    java out formatting q5

---
+++ q5.js
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.print(s1);
+                for(int j=1;j<=15-s1.length();j++){
+                System.out.print(" ");
+                }
+                if(x<10){
+                    System.out.println("00"+x);
+                }    
+                else if(x<100){
+                                  System.out.println("0"+x);
+                }
+                else{System.out.println(x);
+                }
+                              }
+            
+            System.out.println("================================");
+
+    }
+}
+
